64746263a7d04e68cfe408b9866098584032d726
RosetheBard <rothart26@gmail.com>
2026-04-02 22:14:15 -0300

    kdksdvnçosnvuewanbl

--- adsROSEMARRY/aula atual/index3.php
+++ adsROSEMARRY/aula atual/index3.php
@@ -1,3 +1,20 @@
 <?php
+session_start();
 
+define("HOST", "localhost");
+define("USUARIO", "root");
+define("SENHA", "");
+define("BANCO", "rsemarry");
+
+$mysqli = new mysqli(HOST, USUARIO, SENHA, BANCO);
+
+$email = $_SESSION['email'];
+$senha = $_SESSION['senha'];
+
+
+$query = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+$puxa = $mysqli->query($query);
+$mostra = $puxa->fetch_object();
+
+echo "olá" . $mostra->nome;
 ?>
