771e9baf73753cb6f3fe551feb2c2caf3aac3db4
RosetheBard <rothart26@gmail.com>
2026-04-14 21:04:17 -0300

    14 de abril de 2026

--- adsROSEMARRY/aula atual/index.php
+++ adsROSEMARRY/aula atual/index.php
@@ -1,38 +1,33 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link rel="shortcut icon" href="img/lua.png" type="img/lua.png">
+    <link rel="shortcut icon" href="img/lua.png" type="img/lua.png">
     <meta name="description" content="esse é um website de portifolio, com o intuito de divulgar meus trabalhos e estudos">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
     <title>Rose-HomePage</title>
 </head>
-<body>
-    <header class="conteiner">
 
-
-        <header class="row">
-            <header class="col-12 bg-dark">
-                <img src="img/lua.png" class="w-25" alt="logo do portifolio">
-
-
+<body class="conteiner">
+    <header class="row">
+            <header class="col-12 cabecalho">
+                <img src="img/lua.png" class="i-sm mn-head" alt="logo do portifolio">
+                <img src="img/pfbanner.jpg" class="i-sm mn-hv" alt="menu expansivo">
             </header>
         </header>
 
-
-    </header>
-    <section>
-
-    </section>
-    <footer>
-
-    </footer>
+    <main class="corpo">
+        
+    </main>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
         crossorigin="anonymous"></script>
 </body>
+
 </html>
