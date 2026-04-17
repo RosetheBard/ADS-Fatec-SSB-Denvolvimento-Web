043b8669dc866471674c56cf0418f7415779c7f7
RosetheBard <rothart26@gmail.com>
2026-04-16 22:02:10 -0300

    aula do dia 16 de abril de 2026

---
+++ adsROSEMARRY/portifolio/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/lua.png" type="img/lua.png">
+    <meta name="description" content="esse é um website de portifolio, com o intuito de divulgar meus trabalhos e estudos">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Rose-HomePage</title>
+</head>
+
+<body class="conteiner">
+    <header class="row">
+        <div class="col-12 cabecalho">
+            <img src="img/lua.png" class="i-sm mn-head" alt="logo do portifolio">
+            <img src="img/pfbanner.jpg" class="i-sm mn-hv" alt="menu expansivo">
+        </div>
+    </header>
+
+    <main class="row corpo">
+        <div class="col-12">
+            <div class="bx-pink">
+
+                <!-- corpo do portifólio -->
+                <h1 class="text-center">Seja Bem vindo</h1>
+
+                <img src="img/pfbanner.jpg" class="i-bn" alt="banner do portifólio">
+
+                <section>
+                    <!-- sessão de ultimos projetos -->
+
+                    <h2 class="text-center">Ultimos Projetos</h2>
+
+                    <div id="ultimos-projetos" class="carousel slide">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <a href="#"><img src="img/pfbanner-2.jpg" class="d-block w-100" alt="..."></a>
+                            </div>
+                            <div class="carousel-item">
+                                <a href="#"><img src="img/pfbanner-2.jpg" class="d-block w-100" alt="..."></a>
+                            </div>
+                            <div class="carousel-item">
+                                <a href="#"><img src="img/pfbanner-2.jpg" class="d-block w-100" alt="..."></a>
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#ultimos-projetos" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#ultimos-projetos" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </section>
+
+                <!--  -->
+
+            </div>
+        </div>
+    </main>
+
+    <footer class=" row footer">
+        <div class="col-12">
+            <small>&copy; 2026 Rosemarry. todos os reservados</small>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
